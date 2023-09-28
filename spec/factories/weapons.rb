641d879319779b17d633f652a556652d0c7e16e4
vituhonda <vitorutk@gmail.com>
2023-09-28 16:38:37 -0300

    feat(factorybot): add a constructor for weapons

--- spec/factories/weapons.rb
+++ spec/factories/weapons.rb
@@ -2,6 +2,9 @@ FactoryBot.define do
   factory :weapon do
     level {1}
     xp {0}
+    pb {100}
+    ps {Faker::Number.within(range: 10..99)}
+    description {ps <= 50 ? "arma comum" : "arma rara"}
   end
 end
 
