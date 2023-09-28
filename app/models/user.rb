e028bb10b2eeabd065aa0d9cd475eeab4fcc464a
vituhonda <vitorutk@gmail.com>
2023-09-28 09:26:29 -0300

    test(user): adding user test for level verification and title

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,22 @@ class User < ApplicationRecord
   enum kind: [ :knight, :wizard]
   validates :level, numericality: {greater_than: 0, less_than_or_equal_to: 99}
 
+  attr_accessor :nickname
+  attr_accessor :kind
+  attr_accessor :level
+
   def title
-    "#{self.kind} #{self.nickname} #{self.level}"
+    "#{self.kind} #{self.nickname} ##{self.level}"
+  end
+  # def create(nickname, kind, level)
+  #   self.nickname = nickname
+  #   self.kind = kind
+  #   self.level = level
+  #   end
+  def create(nickname, kind, level)
+    @nickname = nickname
+    @kind = kind
+    @level = level
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 require 'spec_helper.rb'
 require 'user'
+require 'faker'
 
 RSpec.describe User, type: :model do
 
+  let(:random) {Faker::Name.name}
+
   it "is valid if the level is not between 1 and 99" do
-
-
-
+    expect(User.create(nickname: 'Chronos Mage', kind: :knight, level: 100)).to_not be_valid
   end
-
 
   it "returns the correct hero title" do
-
     user = User.create(nickname: 'Chronos', kind: :wizard, level: 1)
     expect(user.title).to eq('wizard Chronos #1')
-
   end
 
+  it "testing random name with faker" do
+    user = User.create(nickname: random, kind: :wizard, level: 60)
+    expect(user.level).to eq(60)
+  end
 
 end
