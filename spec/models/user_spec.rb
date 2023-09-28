b203ffe49ba3b87f9fa2b108052925fdf07f184e
vituhonda <vitorutk@gmail.com>
2023-09-28 09:56:07 -0300

    test(user): adding user test with random values for all atributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,9 @@ require 'faker'
 
 RSpec.describe User, type: :model do
 
-  let(:random) {Faker::Name.name}
+  let(:name) {Faker::Name.first_name}
+  let(:kind) {%i[knight wizard].sample}
+  let(:level) {Faker::Number.within(range: 100..9999)}
 
   it "is valid if the level is not between 1 and 99" do
     expect(User.create(nickname: 'Chronos Mage', kind: :knight, level: 100)).to_not be_valid
@@ -17,8 +19,18 @@ RSpec.describe User, type: :model do
   end
 
   it "testing random name with faker" do
-    user = User.create(nickname: random, kind: :wizard, level: 60)
+    user = User.create(nickname: name, kind: :wizard, level: 60)
     expect(user.level).to eq(60)
   end
+
+  it "v2 is valid if the level is not between 1 and 99" do
+    user = User.new(nickname: name, kind: kind, level: level)
+    # puts user.nickname
+    # puts user.kind
+    # puts user.level
+    expect(user).to_not be_valid
+  end
+
+
 
 end
