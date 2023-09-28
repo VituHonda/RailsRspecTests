5b3a2fe0f5d848ab90d92860132238260dbe661c
vituhonda <vitorutk@gmail.com>
2023-09-28 16:38:11 -0300

    test(weapon): add test to check power on start weapon and power after leveing weapon

--- spec/models/weapon_spec.rb
+++ spec/models/weapon_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'spec_helper.rb'
+require 'faker'
 
 RSpec.describe Weapon, type: :model do
 
@@ -9,11 +10,28 @@ RSpec.describe Weapon, type: :model do
     expect(weapon.level).to eq(1)
   end
 
+  it "store extra xp" do
+    weapon.give_xp(350)
+    weapon.up
+    expect(weapon.xp).to_not eq(0)
+  end
+
   it "is not level one" do
     weapon.give_xp(350)
     weapon.up
     expect(weapon.level).to_not eq(1)
   end
+
+  it "start power 100" do
+    expect(weapon.power).to eq(100)
+  end
+
+  it "leveled weapon power" do
+    weapon.give_xp(350)
+    weapon.up
+    expect(weapon.power).to_not eq(100)
+  end
+
 
 
 end
