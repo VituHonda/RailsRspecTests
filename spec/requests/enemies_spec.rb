ea034f1cabd4652bf081f4c376089a369a7201f3
vituhonda <vitorutk@gmail.com>
2023-10-02 16:17:31 -0300

    refactor(enemies): creating let to improve our tests

--- spec/requests/enemies_spec.rb
+++ spec/requests/enemies_spec.rb
@@ -1,34 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Enemies", type: :request do
+  let(:enemy) {create(:enemy)}
+  let(:enemy_attributes) {attributes_for(:enemy)}
+
+  before(:each) {put "/enemies/#{enemy.id}", params: enemy_attributes}
 
   describe "PUT /enemies" do
     context 'when the enemy exists' do
       it 'return status code 200' do
-        enemy = create(:enemy)
-        enemy_attributes = attributes_for(:enemy)
-        put "/enemies/#{enemy.id}", params: enemy_attributes
         expect(response).to have_http_status(200)
       end
-
       it 'updates the enemy' do
-        enemy = create(:enemy)
-        enemy_attributes = attributes_for(:enemy)
-
-        put "/enemies/#{enemy.id}", params: enemy_attributes
-
         expect(enemy.reload).to have_attributes(enemy.attributes)
       end
-
       it 'returns the enemy updated' do
-        enemy = create(:enemy)
-        enemy_attributes = attributes_for(:enemy)
-        put "/enemies/#{enemy.id}", params: enemy_attributes
-        json_response = JSON.parse(response.body)
-        expect(enemy.reload).to have_attributes(json_response.except('created_at', 'updated_at'))
+        expect(enemy.reload).to have_attributes(json.except('created_at', 'updated_at'))
       end
     end
-
     context 'when the enemy does not exist' do
       it 'returns status code 404' do
         put '/enemies/0', params: attributes_for(:enemy)
@@ -40,37 +29,29 @@ RSpec.describe "Enemies", type: :request do
         expect(response.body).to match(/Couldn't find Enemy/)
       end
     end
-
   end
 
   describe 'DELETE /enemies' do
     context 'when the enemy exists' do
       it 'returns status code 204' do
-        enemy = create(:enemy)
         delete "/enemies/#{enemy.id}"
         expect(response).to have_http_status(204)
       end
       it 'destroy the record' do
-        enemy = create(:enemy)
         delete "/enemies/#{enemy.id}"
         expect{enemy.reload}.to raise_error ActiveRecord::RecordNotFound
       end
     end
-
     context 'when the enemy does not exist' do
       it 'returns status code 404' do
         delete '/enemies/0'
         expect(response).to have_http_status(404)
       end
-
       it 'returns a not found message' do
         delete '/enemies/0'
         expect(response.body).to match(/Couldn't find Enemy/)
       end
     end
-
   end
-
-
 
 end
